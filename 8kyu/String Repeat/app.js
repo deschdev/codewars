2eea983aa04fa7a7b995f2b5d81d288f85eeae3c
deschdev <94028614+deschdev@users.noreply.github.com>
2024-06-07 22:11:11 -0400

    8 and a 7kyu

---
+++ 8kyu/String Repeat/app.js
@@ -0,0 +1,17 @@
+/* 
+STRING REPEAT
+
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Ex:
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+*/
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+console.log(repeatStr(3, "*"), "***");
+console.log(repeatStr(5, "#"), "#####");
+console.log(repeatStr(2, "ha "), "ha ha ");
